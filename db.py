724967c6c8ba2b370790f15962e0170e2ee7c96b
Rainbowcat13 <rainbowcat1308@gmail.com>
2020-10-29 15:31:08 +0300

    add documentation to DBConnector class

--- db.py
+++ db.py
@@ -19,19 +19,34 @@ class DBConnector:
 
     def aggregate_one(self, collection_name: str, parameters: Dict,
                       update: Dict = None, delete=False):
+        """
+        Searches, updates or deletes ONE document by given parameters
+        :param collection_name: Name of needed collection
+        :param parameters: Fields for search
+        :param update: Fields for update (if needed)
+        :param delete: Flag for deleting document found
+        :return: Nothing if it is update; Document found if it is search or delete
+        """
         collection = self[collection_name]
         if update is not None:
-            return collection.find_one_and_update(parameters, {'$set': update})
+            collection.find_one_and_update(parameters, {'$set': update})
+            return
         if delete:
             collection.delete_one(parameters)
-            return
         return collection.find_one(parameters)
 
     def aggregate_many(self, collection_name: str, parameters: Dict = None,
                        update: Dict = None, delete=False):
+        """
+        Searches, updates or deletes ALL documents by given parameters
+        :param collection_name: Name of needed collection
+        :param parameters: Fields for search
+        :param update: Fields for update (if needed)
+        :param delete: Flag for deleting documents found
+        :return: Nothing if it is update; All documents found if it is search or delete
+        """
         if parameters is None:
             parameters = {}
-        # print([x for x in self.collections['students'].find(parameters)])
         collection = self[collection_name]
         if update is not None:
             for item in collection.find(parameters):
@@ -42,17 +57,23 @@ class DBConnector:
         return collection.find(parameters)
 
     def add_one(self, collection_name: str, fields: Dict):
+        """
+        Adds one new document to the collection
+        :param collection_name: Name of needed collection
+        :param fields: fields of the document
+        :return: Nothing
+        """
         if self.aggregate_one(collection_name, fields) is None:
             self[collection_name].insert_one(fields)
 
     def add_many(self, collection_name: str, params: Dict[str, List]):
         """
-        :param collection_name: Name of needed collection, it's simple
-        :param params: This shit is working like this -- params have a set of keys and by each key you can get
-                       a list of values. So this insert to collection count=len(params[<any_key>]) elements,
+        Adds many new documents to collection
+        :param collection_name: Name of needed collection
+        :param params: It is a set of keys and by each key you can get
+                       a list of values. So this method inserts to collection count=len(params[<any_key>]) elements,
                        each is a dict of all keys from params and values by all keys by index from 0 to count.
-                       Fuck you I can't explain it properly.
-        :return: Literally nothing
+        :return: Nothing
         """
         size = 0
         for key, value in params.items():
@@ -64,8 +85,18 @@ class DBConnector:
             self.add_one(collection_name, {key: value[i] for key, value in params.items()})
 
     def get_all(self, collection_name):
+        """
+        Gets all documents from collection
+        :param collection_name: Name of needed collection
+        :return: All documents from collection
+        """
         return self[collection_name].find()
 
     def __getitem__(self, collection_name):
+        """
+        Just gets collection by name
+        :param collection_name: Name of needed collection
+        :return: Collection by collecion_name
+        """
         if collection_name in self.collections:
             return self.collections[collection_name]
